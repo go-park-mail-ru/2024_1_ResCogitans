6fc5efc82cd397157e6ecc3d6447aec4bcdf87fc
sp20ks <spas.ksusha@gmail.com>
2024-03-02 21:49:12 +0300

    added wrap for errors

---
+++ cmd/2024_1_ResCogitans/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/go-park-mail-ru/2024_1_ResCogitans/internal/config"
+	"github.com/go-park-mail-ru/2024_1_ResCogitans/internal/http-server/server"
+	"github.com/go-park-mail-ru/2024_1_ResCogitans/router"
+	"github.com/go-park-mail-ru/2024_1_ResCogitans/utils/logger"
+)
+
+func main() {
+	logger := logger.Logger()
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		logger.Error("Failed to load config", "error", err)
+		return
+	}
+
+	logger.Info("Start config", "config", cfg)
+
+	router := router.SetupRouter(cfg)
+
+	if err := server.StartServer(router, cfg); err != nil {
+		logger.Error("Failed to start server", "error", err)
+		return
+	}
+}
